af0ae7bf4fd636d23f6b92b0e5dc4aa8c65d7bf3
harshadmestry <36636686+harshadmestry@users.noreply.github.com>
2018-02-20 20:40:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,17 +5,28 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    title: 'Article One',
-    heading: 'Feb 2018',
-    date:'20 Feb',
-    content:`<p>
-                This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article. 
-            </p>
-            <p>
-                This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.
-            </p>
+var articles = {
+    'article-one' : {
+        title: 'Article One',
+        heading: 'Feb 2018',
+        date:'20 Feb',
+        content:`<p>
+                    This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article. 
+                </p>
+                <p>
+                    This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.
+                </p>
                 `
+},
+    'article-two':{
+        title: 'Article Two',
+        heading: 'Feb 2018',
+        date:'20 Feb',
+        content:`<p>
+                    This is the two article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article.This is the first article. 
+                </p>
+                `
+    }
 };
 function createTemplate (data){
     var title=data.title;
@@ -57,11 +68,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-    res.send(createTemplate(articleOne));
-});
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+app.get('/:articleName',function(req,res){
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+    
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
